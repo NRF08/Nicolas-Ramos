24c251e229937155cadca8b5fe6d035279f4af35
Nicolas Ramos <nicolas.freitas8@etec.sp.gov.br>
2022-11-07 20:42:31 -0300

    Gravaçao inicial

--- Nicolas Ramos/Program.cs
+++ Nicolas Ramos/Program.cs
@@ -10,6 +10,37 @@ namespace Nicolas_Ramos
     {
         static void Main(string[] args)
         {
+            string nome;
+            string email;
+            string endereço;
+            string bairro;
+            string cidade;
+            int telefone;
+            int idade;
+
+            Console.WriteLine("Bem-vindo!");
+            Console.WriteLine("Escreva seu nome completo:");
+        nome: Console.ReadLine();
+
+            Console.WriteLine("Escreva seu email:");
+        email: Console.ReadLine();
+
+            Console.WriteLine("Escreva sua idade:");
+        idade: Console.ReadLine();
+
+            Console.WriteLine("Escreva seu endereço:");
+        endereço: Console.ReadLine();
+
+            Console.WriteLine("Escreva seu bairro:");
+        bairro: Console.ReadLine();
+
+            Console.WriteLine("Escreva sua cidade:");
+        cidade: Console.ReadLine();
+
+            Console.WriteLine("Escreva seu telefone:");
+        telefone: Console.ReadLine();
+
+            Console.ReadKey();
         }
     }
 }
